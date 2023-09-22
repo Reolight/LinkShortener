e9ae0cf56ea2078b81c435f5736c66d7ceb089b4
Reolight <95131769+Reolight@users.noreply.github.com>
2023-09-22 15:26:12 +0300

    Migrations recreated

--- EFCoreStore/Migrations/20230922122420_initial.Designer.cs
+++ EFCoreStore/Migrations/20230922122420_initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EFCoreStore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCoreStore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230922122420_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
